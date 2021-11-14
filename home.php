299f84ef99057bf9ecf18565912e74f328cf0242
Milica Pajic <pajicmilica.31@gmail.com>
2021-11-14 20:47:23 +0100

    Napravljen kostur login stranice i zapoceta home stranica

--- home.php
+++ home.php
@@ -4,10 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/cssHome.css">
     <title>Document</title>
 </head>
 <body>
+ <div>   
+        <table>
+            <tr id="i">
+                <th>Name item</th>
+                <th>Final date</th>
+                <th>Urgency of execution</th>
+            </tr>
+            <tr>
+                <td> </td>
+                <td> </td>
+                <td></td>
+            </tr>
+            
+            
+        </table>
     
+
 
 
 </body>
